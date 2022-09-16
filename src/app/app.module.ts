f67efc4831d53347c4df7409da137eb2fac05cd8
Arun94-HD <arunkumar.hd4@gmail.com>
2022-09-16 08:38:01 +0530

    Merge the code from the master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DemoComponent } from './demo.component';
+import { DataBindComponent } from './data-bind/data-bind.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DemoComponent,
+    DataBindComponent
   ],
   imports: [
     BrowserModule
