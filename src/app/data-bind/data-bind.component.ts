4f4793d754202d1ed72803829d2929e26006e98a
Arun94-HD <arunkumar.hd4@gmail.com>
2022-10-25 16:36:50 +0530

    Simply 2

--- src/app/data-bind/data-bind.component.ts
+++ src/app/data-bind/data-bind.component.ts
@@ -19,47 +19,4 @@ Location: any;
 
   ngOnInit(): void {
   }
-=======
-  styleUrls!: ['./data-bind.component.css']
-  export class DataBindComponent {
-  }
- 
-  controlNumber = 10;
-  controlType = "text";
-
-  constructor() {
-    // this.control();
-  }
-
-  control() {
-    switch(this.controlNumber){
-        case 10 :{
-           this.controlType = "radio";
-           break;
-        }
-        case 20:{
-          this.controlType = "checkbox";
-          break;
-        }
-        default : {
-           this.controlType = "text";
-        }
-    }
-  }
-
-  CourseName = 'Angular_Test_1';
-
-  getCourseName() {
-    return 'Angular_Test_2';
-  }
-
-  Location = 'Bangolore';
-
-
-  Fired(){
-     console.log("it is executing..... Thanks much");
-  }
-
-  evtControlCheck(){
-    this.control();
-  }
+}
